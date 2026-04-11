136f4abbaf33d55fb575cbeca6278a4937512a56
Reyansh Bhootra <reyansh.bhootra@gmail.com>
2026-04-10 20:09:57 -0400

    connect waitlist to brevo

---
+++ netlify/functions/subscribe.js
@@ -0,0 +1,26 @@
+exports.handler = async function(event) {
+  if(event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method not allowed' };
+  }
+
+  const { email } = JSON.parse(event.body);
+
+  const response = await fetch('https://api.brevo.com/v3/contacts', {
+    method: 'POST',
+    headers: {
+      'accept': 'application/json',
+      'content-type': 'application/json',
+      'api-key': process.env.BREVO_API_KEY
+    },
+    body: JSON.stringify({
+      email: email,
+      listIds: [2],
+      updateEnabled: true
+    })
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ success: true })
+  };
+}
